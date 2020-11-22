a57fdd3f6c1bc25edb94c3344c512c9f740c11a4
Priyanshi Somani <somanipriyanshi1996@gmail.com>
2020-11-22 10:52:00 +0530

    build issue fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  isSideNavOpen: boolean;
+  isSideNavOpen: boolean= false;
   constructor() { }
 
   ngOnInit(): void {
