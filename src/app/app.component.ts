e6cb24302345dfdfec9a8cb82509d3cf6153a067
Priyanshi Somani <somanipriyanshi1996@gmail.com>
2020-11-25 09:14:39 +0530

    background image setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToggleStatusEmit } from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'priyanshiPortfolio';
+  scrollToView(scrollData:ToggleStatusEmit){
+    console.log(scrollData);
+  }
 }
